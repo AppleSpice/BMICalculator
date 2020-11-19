f6350b78afe6f6e91b44b5439e60892de0e33676
Gaven Rudiak <gavenrudiak@outlook.com>
2020-11-19 10:42:25 -0500

    Part 3

--- BMICalculator/MainWindow.xaml.cs
+++ BMICalculator/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +14,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace BMICalculator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    [XmlRoot("BMICalculator", Namespace ="www.bmicalc.ninja")]
     public partial class MainWindow : Window
     {
+
+        public string FilePath = "C:/temp/";
+        public string FileName = "yourBMI.xml";
         public class Customer
         {
+            [XmlAttribute("Last Name")]
             public string lastName { get; set; }
+            [XmlAttribute("First Name")]
             public string firstName { get; set; }
+            [XmlAttribute("Phone Number")]
             public string phoneNumber { get; set; }
+            [XmlAttribute("Height")]
             public int heightInches { get; set; }
+            [XmlAttribute("Weight")]
             public int weightLbs { get; set; }
+            [XmlAttribute("Customer BMI")]
             public int custBMI { get; set; }
+            [XmlAttribute("Status")]
             public string statusTitle { get; set; }
         }
 
@@ -36,6 +52,7 @@ namespace BMICalculator
         public MainWindow()
         {
             InitializeComponent();
+            OnLoadStats();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -96,7 +113,32 @@ namespace BMICalculator
                 //normal
                 xBMIText.Text = "According to CDC.gov you are the normal weight.";
             }
-            //MessageBox.Show($"The Customer's name is {customer1.firstName} {customer1.lastName} and they can be reached at {customer1.phoneNumber}. They are {customer1.heightInches} inches tall. Their weight is {customer1.weightLbs}. Their BMI is {bmi}");
+
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+            XmlSerializer ser = new XmlSerializer(typeof(Customer));
+
+            ser.Serialize(writer, customer1);
+            writer.Close();
+
+        }
+
+        private void OnLoadStats()
+        {
+            Customer cust = new Customer();
+
+            XmlSerializer des = new XmlSerializer(typeof(Customer));
+            using (XmlReader reader = XmlReader.Create(FilePath + FileName))
+            {
+                cust = (Customer)des.Deserialize(reader);
+
+                xLastName.Text = cust.lastName;
+                xFirstName.Text = cust.lastName;
+                xPhone.Text = cust.phoneNumber;
+            }
+
+            DataSet xmlData = new DataSet();
+            xmlData.ReadXml(FilePath + FileName, XmlReadMode.Auto);
+            xDataGrid.ItemsSource = xmlData.Tables[0].DefaultView;
         }
     }
 }
